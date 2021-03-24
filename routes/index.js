28237d7c0a15e92b72add49619fabe962a64449d
Makhmud <insite-it@ya.ru>
2021-03-24 15:28:08 +0500

    categories output ready

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { sessionChecker } = require('../middleware/auth');
+const Category = require('../models/category.model');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.render('index', { title: 'App title' });
+  const categories = await Category.find().lean();
+  console.log(categories);
+  res.render('index', { title: 'App title', categories });
 });
 
 module.exports = router;
