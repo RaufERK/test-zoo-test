568ce720d33adab15450b66af9a5329c6691171d
Dima <shetkodv@gmail.com>
2021-03-25 09:48:23 +0300

    error category

--- routes/animals.js
+++ routes/animals.js
@@ -6,8 +6,10 @@ const router = express.Router();
 
 router.get('/category/:categoryName', async (req, res) => {
   const { categoryName } = req.params;
-  const category = await Category.findOne({ englishName: categoryName }).populate('animals');
-  // const animalsWithPics = 
+  const category = await Category.findOne({
+    englishName: categoryName,
+  }).populate('animals');
+  // const animalsWithPics =
   res.render('animals/category', { title: '', category });
 });
 
@@ -15,6 +17,45 @@ router.get('/animals', async (req, res) => {
   const categories = await Category.find();
   res.render('animals/categories', { title: '', categories });
 });
+
+router
+  .route('/animals/edit/:id')
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const animal = await Animal.findById(id).lean();
+    // console.log(animal);
+    const category = await Category.find(); //.lean();
+
+    const curCategory = category.find((el) => el.animals.includes(id));
+    // console.log('curCategory================>>>',curCategory);
+
+    const editCategory = category.map((el) => {
+      const newObj = el.lean();
+
+      newObj.test = 'TEST';
+      console.log(
+        el.title,
+        newObj.title,
+        curCategory.title,
+        newObj.title === curCategory.title ? true : false
+      );
+      newObj.selectedCategory =
+        newObj.title === curCategory.title ? true : false;
+      // console.log(newObj);
+      // console.log('дескрипторы',Object.getOwnPropertyDescriptors(el));
+      return newObj;
+    });
+    console.log('================>', editCategory[2]);
+
+    res.render('admin/editAnimals', {
+      animal,
+      category: editCategory,
+      curCategory,
+    });
+  })
+  .post((req, res) => {
+    console.log(req.body);
+  });
 
 router.get('/animals/:animalName', async (req, res) => {
   const { animalName: englishName } = req.params;
@@ -24,10 +65,5 @@ router.get('/animals/:animalName', async (req, res) => {
   console.log(animal);
   res.render('animals/animal', { title: '', animal });
 });
-
-router.route('animal/:id').get( async (req, res) => {
-  const animal = await Animal.findById(req.params.id);
-  console.log(animal);
-})
 
 module.exports = router;
