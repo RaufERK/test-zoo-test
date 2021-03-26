94fd30a314b0f8b50d99667bc3f55c2d6f029aa2
Dima <shetkodv@gmail.com>
2021-03-26 11:22:54 +0300

    add remove pic

--- public/js/editAnimals.js
+++ public/js/editAnimals.js
@@ -1,5 +1,21 @@
 const animalPic = document.querySelector('#animal-pic');
 
+const editForm = document.querySelector('#animal-form-edit');
+const addPicForm = document.querySelector('.wrap-add-pic');
+
+editForm.addEventListener('click', (e) => {
+  if(e.target.id === 'add-animal-pic'){
+    editForm.style.display = 'none';
+    addPicForm.style.display = 'block';
+  }
+})
+
+addPicForm.addEventListener('click', (e) => {
+  if(e.target.id === 'add-animal-pic'){
+    addPicForm.style.display = 'none';
+    editForm.style.display = 'block';
+  }
+})
 
 animalPic.addEventListener('click', async (e) => {
   if(e.target.classList.contains('preview-close-btn')){
@@ -10,7 +26,7 @@ animalPic.addEventListener('click', async (e) => {
     
     const response = await fetch(`/admin/animals/delete/image/${idAnimal}?srcDel=${srcPicToDelete}`);
     if(response.status === 200){
-      console.log('Картинка удалена');
+      e.target.parentNode.remove();
     }
   }
 })
